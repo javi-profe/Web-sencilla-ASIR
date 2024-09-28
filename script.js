ac05264fb32249cae13d99d2086cf5df4b2cf2e8
David Achaerandio <achacadav@gmail.com>
2024-09-28 12:36:28 +0200

    he añadido una galería de imágenes con funcionalidad modal modificando el js y html

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // script.js
 console.log("Bienvenidos a la página web sencilla.");
+
+var modals = document.querySelectorAll('.modal');
+var closeModalButtons = document.querySelectorAll('.close');
+
+closeModalButtons.forEach(function(button) {
+    button.onclick = function() {
+        this.parentElement.style.display = "none";
+    };
+});
+
+modals.forEach(function(modal) {
+    modal.onclick = function() {
+        this.style.display = "none";
+    };
+});
+
